71475e5e8686d51cd9a37b09357ff99a17c0572d
xosedelara <97544706+xosedelara@users.noreply.github.com>
2022-05-24 17:13:56 -0500

    configuración de tipografía

--- burger-queen/src/Firebase/firebaseFirestore.js
+++ burger-queen/src/Firebase/firebaseFirestore.js
@@ -7,21 +7,8 @@ import { app } from './firebaseApp';
 
 const db = getFirestore(app);
 const empleadxs = collection(db, 'Empleadxs');
-// eslint-disable-next-line no-unused-vars
-// eslint-disable-next-line import/no-mutable-exports
 
 export const getDocuments = async () => {
   const documents = await getDocs(empleadxs);
   return documents;
 };
-// eslint-disable-next-line import/no-mutable-exports
-// const active = query((empleadxs), where('id', '==', activeUser));
-/* const getEmployees = (activeUser) => {
-  const active = query((empleadxs), where(empleadxs, '===', activeUser));
-  console.log(active, 'firestoreeeee');
-  getDocs(active).then((docs) => {
-    const documentFound = docs.find((doc) => active === doc.id);
-    console.log(documentFound, 'in getEmployees');
-  });
-};
- */
